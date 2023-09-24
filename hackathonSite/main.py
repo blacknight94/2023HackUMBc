bc11e08a3ce8566e10b843876732f611e00435c6
SJK146 <66105006+SJK146@users.noreply.github.com>
2023-09-24 10:14:50 -0400

    Update main.py

--- hackathonSite/main.py
+++ hackathonSite/main.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 from flask import Flask, request, render_template
 
 import apiFunctions
+import hellDiver
 
 app = Flask(__name__)
 
@@ -10,8 +11,9 @@ app = Flask(__name__)
 def index():
     dynamic_data = {"Key": "Item", "key2": "item2"}
     if request.method == 'POST':
-        print("here ")
         data_from_form = request.form['submit']
+
+        dynsmic_data = esgAPI(data_from_form)
         dynamic_data = apiFunctions.esgAPI(data_from_form)
         return render_template('index.html', dynamic_data=dynamic_data)
         # Process the data (e.g., save it, perform an action, etc.)
